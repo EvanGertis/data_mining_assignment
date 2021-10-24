52aab5a8f2ce302b69655e6830a62732a248f706
EvanGertis <physevangertis@gmail.com>
2021-10-24 15:25:32 -0400

    add entropy discretization file

--- entropy_discretization.py
+++ entropy_discretization.py
@@ -25,53 +25,113 @@ def entropy_discretization(s):
 
     I = {}
     i = 0
-    while(uniqueValue(s)):
+    n = s.nunique()['Class']
+    s1 = pd.DataFrame()
+    s2 = pd.DataFrame()
+    distinct_values = s['A1'].value_counts().index
+    information_gain_indicies = []
+    print(f'The unique values for dataset s["A1"] are {distinct_values}')
+    for i in distinct_values:
+
         # Step 1: pick a threshold
-        threshold = s['A1'].iloc[0]
+        threshold = i
+        print(f'Using threshold {threshold}')
 
         # Step 2: Partititon the data set into two parttitions
-        s1 = s[s['A1'] < threshold]
+        s1 = s[s['Class'] < threshold]
         print("s1 after spitting")
         print(s1)
         print("******************")
-        s2 = s[s['A1'] >= threshold]
+        s2 = s[s['Class'] >= threshold]
         print("s2 after spitting")
         print(s2)
         print("******************")
-            
+
+        print("******************")
+        print("calculating maxf")
+        print(f" maxf {maxf(s['Class'])}")
+        print("******************")
+
+        print("******************")
+        print("calculating minf")
+        print(f" maxf {minf(s['Class'])}")
+        print("******************")
+
         # Step 3: calculate the information gain.
         informationGain = information_gain(s1,s2,s)
         I.update({f'informationGain_{i}':informationGain,f'threshold_{i}': threshold})
         print(f'added informationGain_{i}: {informationGain}, threshold_{i}: {threshold}')
-        s = s[s['A1'] != threshold]
-        i += 1
+        information_gain_indicies.append(i)
+
+        print(f"Checking condition a if {s1.nunique()['Class']} == {1}")
+        if (s1.nunique()['Class'] == 1):
+            break
+
+        print(f"Checking condition b  {maxf(s1['Class'])}/{minf(s1['Class'])} < {0.5} {s1.nunique()['Class']} == {floor(n/2)}")
+        if (maxf(s1['Class'])/minf(s1['Class']) < 0.5) and (s1.nunique()['Class'] == floor(n/2)):
+            print(f"Condition b is met{maxf(s1['Class'])}/{minf(s1['Class'])} < {0.5} {s1.nunique()['Class']} == {floor(n/2)}")
+            break
+
+
+    print("Elements in I")
+    print(I)
+    print("*****************************")
 
     # Step 5: calculate the min information gain
     n = int(((len(I)/2)-1))
-    print("Calculating minimum threshold")
+    print("Calculating maximum threshold")
     print("*****************************")
-    minInformationGain = 0
-    minThreshold       = 0 
-    for i in range(0, n):
-        if(I[f'informationGain_{i}'] < minInformationGain):
-            minInformationGain = I[f'informationGain_{i}']
-            minThreshold       = I[f'threshold_{i}']
+    maxInformationGain = 0
+    maxThreshold       = 0 
+    for i in information_gain_indicies:
+        print(f"if({I[f'informationGain_{i}']} > {maxInformationGain})")
+        if(I[f'informationGain_{i}'] > maxInformationGain):
+            maxInformationGain = I[f'informationGain_{i}']
+            maxThreshold       = I[f'threshold_{i}']
 
-    print(f'minThreshold: {minThreshold}, minInformationGain: {minInformationGain}')
+    print(f'maxThreshold: {maxThreshold}, maxInformationGain: {maxInformationGain}')
+
+    # replace values
+    print(f" {s1['A1'].value_counts().index}")
+    for i in s1['A1'].value_counts().index:
+        print(f"s1['A1'].replace({i},1)")
+        s1['A1'] = s1['A1'].replace(i,1)
+
+    print(f" {s2['A1'].value_counts().index}")
+    for i in s2['A1'].value_counts().index:
+        print(f"s2['A1'].replace({i},2)")
+        s2['A1'] = s2['A1'].replace(i,2)
+
+    print("s1 after replacing values")
+    print(s1)
+    print("******************")
+    print("s2 after replacing values")
+    print(s2)
+    print("******************")
+    
+
+    partitions = [s1,s2]
+    s = pd.concat(partitions)
 
     # Step 6: keep the partitions of S based on the value of threshold_i
-    minPartition(minInformationGain,minThreshold,s,s1,s2)
+    return s #maxPartition(maxInformationGain,maxThreshold,s,s1,s2)
+
+def maxf(s):
+    return s.max()
+
+def minf(s):
+    return s.min()
 
 def uniqueValue(s):
     # are records in s the same? return true
-    if s.nunique()['A1'] == 1:
+    if s.nunique()['Class'] == 1:
         return False
     # otherwise false 
     else:
         return True
 
-def minPartition(minInformationGain,minThreshold,s,s1,s2):
-    print(f'informationGain: {minInformationGain}, threshold: {minThreshold}')
+def maxPartition(maxInformationGain,maxThreshold,s,s1,s2):
+    print(f'informationGain: {maxInformationGain}, threshold: {maxThreshold}')
     merged_partitions =  pd.merge(s1,s2)
     merged_partitions =  pd.merge(merged_partitions,s)
     print("Best Partition")
@@ -85,13 +145,13 @@ def minPartition(minInformationGain,minThreshold,s,s1,s2):
 
 def information_gain(s1, s2, s):
     # calculate cardinality for s1
-    cardinalityS1 = len(pd.Index(s1['A1']).value_counts())
+    cardinalityS1 = len(pd.Index(s1['Class']).value_counts())
     print(f'The Cardinality of s1 is: {cardinalityS1}')
     # calculate cardinality for s2
-    cardinalityS2 = len(pd.Index(s2['A1']).value_counts())
+    cardinalityS2 = len(pd.Index(s2['Class']).value_counts())
     print(f'The Cardinality of s2 is: {cardinalityS2}')
     # calculate cardinality of s
-    cardinalityS = len(pd.Index(s['A1']).value_counts())
+    cardinalityS = len(pd.Index(s['Class']).value_counts())
     print(f'The Cardinality of s is: {cardinalityS}')
     # calculate informationGain
     informationGain = (cardinalityS1/cardinalityS) * entropy(s1) + (cardinalityS2/cardinalityS) * entropy(s2)
@@ -128,5 +188,5 @@ def entropy(s):
         ent += -pi*log2(pi)
 
     return ent
-
+    
 main()
